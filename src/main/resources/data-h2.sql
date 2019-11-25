60b38f830f8ad8757a392e18e26bbdc0f0c777d5
c1888180 <Randle-JolliffeT@Cardiff.ac.uk>
2019-11-25 21:22:24 +0000

    SQL mock data insert fix

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -7,5 +7,5 @@ INSERT INTO agency (id, agency_name, email) VALUES (1L,'agency1', 'xxx@hotmail.c
 INSERT INTO agency (id, agency_name, email) VALUES (2L,'agency2', 'xxx@sky.com');
 
 
-INSERT INTO timesheet (id, contractor_id, agency_id, days_worked, overtime, week_finish) VALUES (1L, 1, 1, 5, 1, '12/12/2019');
-INSERT INTO timesheet (id, contractor_id, agency_id, days_worked, overtime, week_finish) VALUES (2L, 2, 2, 4, 2, '12/11/2019');
+INSERT INTO timesheet (id, contractor_id, agency_id, days_worked, overtime, week_finish) VALUES (1L, 1, 1, 5, 1, '2019-11-11');
+INSERT INTO timesheet (id, contractor_id, agency_id, days_worked, overtime, week_finish) VALUES (2L, 2, 2, 4, 2, '2019-11-12');
