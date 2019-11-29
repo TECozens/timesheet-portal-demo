69907e79586abd52567af5b989820eb1ccd3774d
c1835316 <camilleril2@cardiff.ac.uk>
2019-11-29 16:19:02 +0000

    creating validation

---
+++ src/main/java/com/admiral/tsp/timesheetportal/review/forms/ApprovalReviewForm.java
@@ -0,0 +1,37 @@
+package com.admiral.tsp.timesheetportal.review.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApprovalReviewForm {
+
+    @NotNull(message = "Must not be empty!")
+    @Max(value = 3, message = "You must choose a number between 1 and 3!")
+    @Min(value = 1, message = "You must choose a number between 1 and 3!")
+    private Integer communication;
+    @NotNull(message = "Must not be empty!")
+    @Max(value = 3, message = "You must choose a number between 1 and 3!")
+    @Min(value = 1, message = "You must choose a number between 1 and 3!")
+    private Integer technical_skills;
+    @NotNull(message = "Must not be empty!")
+    @Max(value = 3, message = "You must choose a number between 1 and 3!")
+    @Min(value = 1, message = "You must choose a number between 1 and 3!")
+    private Integer quality;
+    @NotNull(message = "Must not be empty!")
+    @Max(value = 3, message = "You must choose a number between 1 and 3!")
+    @Min(value = 1, message = "You must choose a number between 1 and 3!")
+    private Integer initative;
+    @NotNull(message = "Must not be empty!")
+    @Max(value = 3, message = "You must choose a number between 1 and 3!")
+    @Min(value = 1, message = "You must choose a number between 1 and 3!")
+    private Integer working_relations;
+
+}
