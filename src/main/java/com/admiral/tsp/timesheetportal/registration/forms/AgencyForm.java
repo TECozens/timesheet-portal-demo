29e8284e05690b02bb5320aa7ef428a1a7b41fff
c1888180 <Randle-JolliffeT@Cardiff.ac.uk>
2019-12-05 00:06:28 +0000

    Design tweaks and validation, display changes

--- src/main/java/com/admiral/tsp/timesheetportal/registration/forms/AgencyForm.java
+++ src/main/java/com/admiral/tsp/timesheetportal/registration/forms/AgencyForm.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -11,10 +13,13 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 public class AgencyForm {
 
-    @NotNull(message = "Must have a username")
+    @NotNull(message = "Must have an Agency name!")
+    @NotBlank(message = "Must have an Agency name!")
     private String name;
 
-    @NotNull(message = "Must have a first name")
+    @Email(message = "Needs to be an email!")
+    @NotNull(message = "Must have an email!")
+    @NotBlank(message = "Must have an email!")
     private String email;
 
 }
